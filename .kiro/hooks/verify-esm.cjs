31c0f2201f75e895130969d6d2c30ffe7de7d106
reprodev <8764255+reprodev@users.noreply.github.com>
2026-08-22 13:15:12 +0100

    kiro: first hook template (verify-esm, PreTaskExec trigger)

---
+++ .kiro/hooks/verify-esm.cjs
@@ -0,0 +1,19 @@
+﻿const fs = require('fs');
+const path = require('path');
+
+const pkgPath = path.resolve(__dirname, '../../package.json');
+
+if (!fs.existsSync(pkgPath)) {
+  process.stderr.write('package.json not found');
+  process.exit(2);
+}
+
+const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'));
+
+if (pkg.type !== 'module') {
+  process.stderr.write('package.json missing "type": "module"');
+  process.exit(2);
+}
+
+process.stdout.write('package.json OK: type=module');
+process.exit(0);
